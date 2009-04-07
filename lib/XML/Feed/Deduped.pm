afe574bc1f976e5c9a8dc4e0f0eade47331c3454
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-04-07 12:02:20 +0900

    initial commit

---
+++ lib/XML/Feed/Deduped.pm
@@ -0,0 +1,33 @@
+package XML::Feed::Deduped;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+XML::Feed::Deduped -
+
+=head1 SYNOPSIS
+
+  use XML::Feed::Deduped;
+
+=head1 DESCRIPTION
+
+XML::Feed::Deduped is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom  slkjfd gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
